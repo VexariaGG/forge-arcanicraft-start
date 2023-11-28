04452b449017c798650c4e832efc2352071e2961
vexariaGG <vexaria@outlook.com.au>
2023-11-28 16:58:11 +1100

    Added Arcanite crystak

--- src/main/java/net/vex/arcanicraft/Arcanicraft.java
+++ src/main/java/net/vex/arcanicraft/Arcanicraft.java
@@ -1,6 +1,7 @@
 package net.vex.arcanicraft;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.vex.arcanicraft.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -25,6 +27,8 @@ public class Arcanicraft {
     private static final Logger LOGGER = LogUtils.getLogger();
     public Arcanicraft() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -40,6 +44,10 @@ public class Arcanicraft {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.ARCANITE);
+
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
